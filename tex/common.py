39e09378e946f3ebc08a1af107b7bdb4c77bb99a
Xiaoyong Guo <xguo@prestolabs.io>
2018-10-09 22:21:51 +0800

    remove E11

--- tex/common.py
+++ tex/common.py
@@ -1,15 +1,5 @@
 import collections
 from recordclass import recordclass
-from gbm import RandomProcess, get_series
-
-
-def which_case(x=0,y=0,C1=0,C2=0,A=0,T=0,a=0,b=0,p1=0,p2=0,dt=0, **kwargs):
-  #print = lambda x:None
-  if p1 + a + A < p2 - T:
-    print('compare C1 and C2: %s, %s' % (C1, C2))
-  else:
-    print('compare b and A+C1: %s, %s' % (b, A+C1))
-
 
 def F(x=0,y=0,C1=0,C2=0,A=0,T=0,a=0,b=0,p1=0,p2=0,dt=0, **kwargs):
   return x * (1-x) * ((C2-C1) + (-A+b-C2)*y)
@@ -19,32 +9,30 @@ def G(x=0,y=0,C1=0,C2=0,A=0,T=0,a=0,b=0,p1=0,p2=0,dt=0, **kwargs):
   return y * (1-y) * ((p1-p2+a) + (A+T)*x)
 
 
-def E11(x=0,y=0,C1=0,C2=0,A=0,T=0,a=0,b=0,p1=0,p2=0,dt=0, **kwargs):
-  return y * (-A+b-C1) + (1-y)*(-C1)
+class RandomProcess(object):
+  ParamType = None
+  StateType = recordclass('Simple_state', ['s', 't'])
 
+  def __init__(self, param, state):
+    self._param = param
+    self._state = state
 
-def E12(x=0,y=0,C1=0,C2=0,A=0,T=0,a=0,b=0,p1=0,p2=0,dt=0, **kwargs):
-  return (1-y)*(-C2)
+  def __iter__(self):
+    return type(self)(self.param, self.state)
 
+  @classmethod
+  def create(cls, **kwargs):
+    param_kwargs = {key: value for key, value in kwargs.items() if key in cls.ParamType._fields}
+    state_kwargs = {key: value for key, value in kwargs.items() if key in cls.StateType._fields}
+    return cls(cls.ParamType(**param_kwargs), cls.StateType(**state_kwargs))
 
-def E1(x=0,y=0,C1=0,C2=0,A=0,T=0,a=0,b=0,p1=0,p2=0,dt=0, **kwargs):
-  e11 = E11(**locals())
-  e12 = E12(**locals())
-  return x*e11 + (1-x)*e12
+  @property
+  def param(self):
+    return self._param
 
-
-def E21(x=0,y=0,C1=0,C2=0,A=0,T=0,a=0,b=0,p1=0,p2=0,dt=0, **kwargs):
-  return x * (p1+A+a) + (1-x)*(p1+a)
-
-
-def E22(x=0,y=0,C1=0,C2=0,A=0,T=0,a=0,b=0,p1=0,p2=0,dt=0, **kwargs):
-  return x*(p1-T)+(1-x)*p2
-
-
-def E2(x=0,y=0,C1=0,C2=0,A=0,T=0,a=0,b=0,p1=0,p2=0,dt=0, **kwargs):
-  e21 = E21(**locals())
-  e22 = E22(**locals())
-  return y*e21 + (1-y)*e22
+  @property
+  def state(self):
+    return self._state
 
 
 class Evo(RandomProcess):
