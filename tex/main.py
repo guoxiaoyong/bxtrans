8a61204c7004f9c918a1c4e3e8a2a83d5ec410cc
Xiaoyong Guo <xguo@prestolabs.io>
2018-10-21 09:38:31 +0800

    update

--- tex/main.py
+++ tex/main.py
@@ -6,6 +6,12 @@ import yaml
 
 from common import get_sim_results
 
+symbol_map = {
+  'A': '$A$',
+  'b': '$b$',
+  'p1': '$\pi_1$',
+  'C2': '$C_2$',
+}
 
 def get_config(case_name):
   with open('cases.yml') as infile:
@@ -18,6 +24,11 @@ def get_config(case_name):
   else:
     raise ValueError('case not found!')
 
+  for k, v in config.items():
+    if isinstance(v, list):
+      label = k
+      break
+
   keys = []
   values = []
   for k, v in config.items():
@@ -26,12 +37,14 @@ def get_config(case_name):
 
   res = []
   for v in itertools.product(*values):
-    res.append(dict(zip(keys, v)))
+    cfg = dict(zip(keys, v))
+    cfg['label'] = '%s=%s' % (symbol_map[label], cfg[label])
+    res.append(cfg)
   return res
 
 
 def main():
-  markers = itertools.cycle('oDs*')
+  markers = itertools.cycle('oD^px')
   configs = get_config(sys.argv[1])
   plt.figure()
   for params in configs:
@@ -39,12 +52,17 @@ def main():
     t = [v['t'] for v in res]
     x = [v['x'] for v in res]
     y = [v['y'] for v in res]
-    plt.plot(t, x, marker=next(markers))
-    plt.plot(t, y, linestyle='--')
-  plt.ylabel('$x$')
+    marker = next(markers)
+    plt.plot(t, x, marker=marker, markevery=10, markersize=4, label='$x$, %s' % params['label'])
+    plt.plot(t, y, linestyle='--', marker=marker, markevery=10, markersize=4, label='$y$, %s' % params['label'])
+  plt.ylabel('$x$ and $y$')
   plt.xlabel('Time($t$)')
   plt.legend(loc='center right')
   plt.grid()
+  #plt.xticks(np.arange(0, 31, step=2))
+  plt.yticks(np.arange(0, 1.2, step=0.1))
+  plt.ylim(0, 1.02)
+  plt.xlim(left=0)
   plt.show()
 
 
